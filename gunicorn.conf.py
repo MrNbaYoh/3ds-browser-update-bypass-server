c1e0762fc150659c74788ee5ac357f54419af323
Nicolas Bailluet <nicolas.bailluet@ens-rennes.fr>
2020-12-20 20:40:30 +0100

    Add .gitignore and SSL parameters for gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,11 @@
 import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2
+
+##### CUSTOM PARAMETERS #####
+
+# certificate for the CDN server
+#certfile =
+
+# private key for the CDN server
+#keyfile =
